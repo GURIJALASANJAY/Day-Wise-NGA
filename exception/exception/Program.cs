48b8c6ca94d796d476488f5b45779ccaeb71031d
Gurijala Sanjay <your-github-email@example.com>
2026-04-20 15:39:22 +0530

    Day 5 - Clean project (removed .vs)

---
+++ exception/exception/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a value:");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter b value:");
+            int b = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine("Result: " + (a / b));
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Exception is occurring due to division by zero");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input. Please enter numbers only.");
+            }
+            finally
+            {
+                Console.WriteLine("hi");
+            }
+        }
+    }
+}
