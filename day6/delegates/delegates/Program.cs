191c53664212b60691d6ea22ca590b71ade02bd0
Gurijala Sanjay <gurijalasanjay841@gmail.com>
2026-04-22 09:27:41 -0700

    Added Day6 clean project

---
+++ day6/delegates/delegates/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    delegate void MathOperation(int a, int b);
+
+    static void Add(int a, int b)
+    {
+        Console.WriteLine("Add: " + (a + b));
+    }
+
+    static void Subtract(int a, int b)
+    {
+        Console.WriteLine("Subtract: " + (a - b));
+    }
+
+    static void Multiply(int a, int b)
+    {
+        Console.WriteLine("Multiply: " + (a * b));
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        MathOperation operation = Add;
+        operation += Subtract;
+        operation += Multiply;
+
+        operation(a, b);
+    }
+}
